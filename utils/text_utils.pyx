ce07ebbe2abf82e227edda229f35a9f8127902a2
Hoang Nghia Tuyen <hnt4499@gmail.com>
2020-01-16 01:31:51 +0530

    Turn all functions into C-only

--- utils/text_utils.pyx
+++ utils/text_utils.pyx
@@ -10,7 +10,7 @@ split_periods = re.compile("(?<=\.{2})\s*(?=[A-Z]+)")
 re_punct = re.compile("[^\w\s]")
 re_digit = re.compile("[\d]")
 
-cpdef list sent_tokenizer(list text):
+cdef list sent_tokenizer(list text):
     """Take a list of lines as input and produce a list of sentences.
 
     Parameters
@@ -59,7 +59,7 @@ cdef str remove(str text):
     return ans
 
 
-cpdef list word_tokenizer(str text):
+cdef list word_tokenizer(str text):
     """Take one sentence as input and produce a list of tokens.
 
     Parameters
