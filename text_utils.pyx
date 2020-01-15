bba4ff146e66025b3960c7bfa47ccdaa66b04286
Hoang Nghia Tuyen <hnt4499@gmail.com>
2020-01-15 19:36:06 +0530

    Add function to remove (truncate) whitespaces

--- text_utils.pyx
+++ text_utils.pyx
@@ -5,7 +5,10 @@ import re
 
 from underthesea import sent_tokenize, word_tokenize
 
+# Compile re modules
 split_periods = re.compile("(?<=\.{2})\s*(?=[A-Z]+)")
+re_punct = re.compile("[^\w\s]")
+re_digit = re.compile("[\d]")
 
 cpdef list sent_tokenizer(list text):
     """Take a list of lines as input and produce a list of sentences.
@@ -34,4 +37,23 @@ cpdef list sent_tokenizer(list text):
         ts = split_periods.split(t)
         for u in ts:
             ans.append(u)
+    return ans
+
+
+cpdef str remove(str text):
+    """Remove whitespaces and digits from text.
+
+    Parameters
+    ----------
+    text : str
+
+    Returns
+    -------
+    str
+        Cleaned text.
+
+    """
+    cdef str ans
+    ans = re_punct.sub("", text)
+    ans = re_digit.sub("", ans)
     return ans
